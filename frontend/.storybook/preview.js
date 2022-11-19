b53bae9255066eb7b1d142d4e7e9d71cadb46201
Bartlomiej Galicki <bartlomiej.galicki.sp@lhsystems.com>
2022-11-20 00:25:43 +0100

    initialize project structure and configuration

---
+++ frontend/.storybook/preview.js
@@ -0,0 +1,12 @@
+// global styles importet for all the stories
+import "../styles/globals.css";
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+}
---
+++ frontend/stories/pages/Home.stories.tsx
@@ -0,0 +1,9 @@
+// Component
+import Home from "../../pages/index";
+
+export const HomePage = () => <Home />;
+
+export default {
+  title: "Pages/Home",
+  component: Home,
+};
